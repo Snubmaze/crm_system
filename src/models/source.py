f269081df8d91d9eb856f9b5ff7488bf97138632
Pavel Polyarus <keytohockey@gmail.com>
2025-11-28 01:39:52 +0300

    Small model Source fix

--- src/models/source.py
+++ src/models/source.py
@@ -6,7 +6,7 @@ class Source(Base):
     __tablename__ = "sources"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(uniq=True, nullable=False)
 
     tickets: Mapped[list["Ticket"]] = relationship(
         back_populates="source",
